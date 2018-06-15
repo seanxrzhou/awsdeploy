4e7b5b557f836c2b5822cb51a4ea787007d57938
Sean Zhou <seanxrzhou@gmail.com>
2018-06-15 13:19:09 -0400

    Remove hardcoding parameters

--- was8deploy.sh
+++ was8deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -ex
 
+INSTALL=$1
+DBARCHIVE=$2
+JEEEAR=$3
+
 mkdir -p /app/ibm/
 cd /tmp
-unzip was8.zip -d /app/ibm/
-unzip pbw-db.jar -d /app/ibm/was8/derby/databases
+unzip $INSTALL -d /app/ibm/
+unzip $DBARCHIVE -d /app/ibm/was8/derby/databases
 rm -f /app/ibm/was8/profiles/AppSrv01/monitoredDeployableApps/servers/server1/*
-cp pbw-ear.ear /app/ibm/was8/profiles/AppSrv01/monitoredDeployableApps/servers/server1
+cp $JEEEAR /app/ibm/was8/profiles/AppSrv01/monitoredDeployableApps/servers/server1
 cd /app/ibm/was8/profiles/AppSrv01/bin
 ./startServer.sh server1
 #
